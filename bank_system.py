e347bd2d1b62eba3d29510c3c5fd43c6c451ff50
damidaramola <damidaramola7@gmail.com>
2024-06-03 20:37:46 +0100

    add bank class

--- bank_system.py
+++ bank_system.py
@@ -10,3 +10,21 @@ class Transaction:
     
     def display_info(self):
         return f"Transaction:\n Expense:{self.title}\n Amount:{self.amount}\n Type:{self.type}\n Note:{self.note}"    
+    
+    
+class Bank:
+    def __init__(self):
+        self.wallet = [] # initialize your wallet
+    #add   
+    def add_transaction(self,transaction): 
+        self.wallet.append(transaction)
+        
+    #remove an existing transaction
+    def del_transaction(self,title):
+        for trans in self.wallet:
+            if trans.title == title:
+                self.wallet.remove(trans)
+                return f"{title} has been removed!"
+        return f"{title} is not found..."    
+    
+        
